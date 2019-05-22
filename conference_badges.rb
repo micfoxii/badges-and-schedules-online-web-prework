3f15ce2eddc34bced687c4f290870f3f47c840bf
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 17:43:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,4 +13,4 @@ end
 
 def assign_rooms(speaker)
   room_assignment = []
-  speaker.each_with_index do {|speaker, index|
+  speaker.each_with_index do {|speaker, index| room_assignment << "Hello, "
